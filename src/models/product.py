60695da1110e850ef8b04f5ca5fab24425f4ced9
Carlos Marra <carlos.marra33@gmail.com>
2021-10-31 17:59:13 -0300

    mudança na classe de produto

--- src/models/product.py
+++ src/models/product.py
@@ -8,30 +8,30 @@ class Product():
         self._qtd = qtd
         self._category = category
 
-        def get_name(self):
-            return self._name
-        def set_name(self, name):
-            self._name = name
-        def get_img(self):
-            return self._img
-        def set_img(self, img):
-            self._img = img
-        def get_description(self):
-            return self._description
-        def set_description(self, description):
-            self.description = description
-        def get_price(self):
-            return self._price
-        def set_price(self, price):
-            self._price = price
-        def get_qtd(self):
-            return self._qtd
-        def set_qtd(self, qtd):
-            self._qtd = qtd
-        def get_category(self):
-            return self._category
-        def set_category(self, category):
-            self._category = category
+    def get_name(self):
+        return self._name
+    def set_name(self, name):
+        self._name = name
+    def get_img(self):
+        return self._img
+    def set_img(self, img):
+        self._img = img
+    def get_description(self):
+        return self._description
+    def set_description(self, description):
+        self.description = description
+    def get_price(self):
+        return self._price
+    def set_price(self, price):
+        self._price = price
+    def get_qtd(self):
+        return self._qtd
+    def set_qtd(self, qtd):
+        self._qtd = qtd
+    def get_category(self):
+        return self._category
+    def set_category(self, category):
+        self._category = category
 
     def print(self):
         print("name = ", self.name)
